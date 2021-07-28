52123e269c6936770b93d3a492b11ec2764b2732
myungkang2020 <kmkaero3746@gmail.com>
2021-07-28 22:36:40 +0900

    Add tail
    * add f, r key

--- moveRect/moveRectConsole/Actor.cpp
+++ moveRect/moveRectConsole/Actor.cpp
@@ -7,25 +7,23 @@ moveRectconsole::Actor::Actor()
 
 int32_t moveRectconsole::Actor::update()
 {
-	
-	Display();
+	using namespace std::chrono;
+
+	if (duration_cast<milliseconds>(system_clock::now() - prevFrameTime) > static_cast<milliseconds>(60)) {
+		move();
+		saveBodyPosData();
+		Display();
+		prevFrameTime = system_clock::now();
+	}
 
 	return int32_t(0);
 }
 
 int32_t moveRectconsole::Actor::Display()
 {
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), headPos);
+	std::cout << 'o' << std::endl;
 
-	COORD prevPos = pos;
-	move();
-	if(prevPos.X != pos.X || prevPos.Y != pos.Y)
-	{
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), prevPos);
-		std::cout << " " << std::endl;
-
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
-		std::cout << "o" << std::endl;
-	}
 
 
 	return int32_t(0);
@@ -33,8 +31,12 @@ int32_t moveRectconsole::Actor::Display()
 
 int32_t moveRectconsole::Actor::init()
 {
-	pos.X = 30;
-	pos.Y = 20;
+	headPos.X = 30;
+	headPos.Y = 20;
+	currentBodyLength = 1;
+	isPrevAddBody = false;
+	bodyPos.push_front({ headPos.X, headPos.Y });
+	prevFrameTime = std::chrono::system_clock::now();
 
 	// Ä¿¼­ ±ôºýÀÓ Á¦°Å
 	static HANDLE hOut;
@@ -45,7 +47,7 @@ int32_t moveRectconsole::Actor::init()
 	cInfo.bVisible = false;
 	SetConsoleCursorInfo(hOut, &cInfo);
 
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), headPos);
 	std::cout << "o" << std::endl;
 
 	return int32_t(0);
@@ -53,25 +55,71 @@ int32_t moveRectconsole::Actor::init()
 
 int32_t moveRectconsole::Actor::move()
 {
-	char input = getInput();
-	if ((GetAsyncKeyState(87) & 0x0001) || GetAsyncKeyState(119) & 0x0001) { pos.Y--; } // W,w
-	if ((GetAsyncKeyState(65) & 0x0001) || GetAsyncKeyState(97) & 0x0001) { pos.X--; } // W,w
-	if ((GetAsyncKeyState(83) & 0x0001) || GetAsyncKeyState(115) & 0x0001) { pos.Y++; } // W,w
-	if ((GetAsyncKeyState(68) & 0x0001) || GetAsyncKeyState(100) & 0x0001) { pos.X++; } // W,w
+	static uint16_t moveWay = 0;
 
-	pos.X = pos.X < 0 ? 0 : pos.X;
-	pos.Y = pos.Y < 0 ? 0 : pos.Y;
+	if ((GetAsyncKeyState(87) & 0x0001) || GetAsyncKeyState(119) & 0x0001) {  moveWay = 1; } // W,w
+	if ((GetAsyncKeyState(65) & 0x0001) || GetAsyncKeyState(97) & 0x0001) {  moveWay = 2; } // a,a
+	if ((GetAsyncKeyState(83) & 0x0001) || GetAsyncKeyState(115) & 0x0001) {  moveWay = 3; } // s,s
+	if ((GetAsyncKeyState(68) & 0x0001) || GetAsyncKeyState(100) & 0x0001) { moveWay = 4; } // d,d
+
+	if ((GetAsyncKeyState(70) & 0x0001) || GetAsyncKeyState(102) & 0x0001) { currentBodyLength++; } // F,f
+	if ((GetAsyncKeyState(82) & 0x0001) || GetAsyncKeyState(114) & 0x0001) { if (currentBodyLength > 1)currentBodyLength--; } // R,r
+
+	switch (moveWay)
+	{
+	case 1:
+		headPos.Y--;
+		break;
+	case 2:
+		headPos.X--;
+		break;
+	case 3:
+		headPos.Y++;
+		break;
+	case 4:
+		headPos.X++;
+		break;
+	default:
+		break;
+	}
+
+	headPos.X = headPos.X < 0 ? 0 : headPos.X;
+	headPos.Y = headPos.Y < 0 ? 0 : headPos.Y;
 
 	return 0;
 }
 
 char moveRectconsole::Actor::getInput()
 {
-	char inputKey = 0;
+	static char inputKey = 0;
 	if (_kbhit())
 	{
 		inputKey = _getch();
 	}
 
 	return char(inputKey);
+}
+
+int32_t moveRectconsole::Actor::saveBodyPosData()
+{
+	uint32_t nowBodyLength = bodyPos.size();
+
+	if (bodyPos.at(0).first != headPos.X || bodyPos.at(0).second != headPos.Y)
+	{
+		bodyPos.push_front({ headPos.X, headPos.Y });
+		if (nowBodyLength < currentBodyLength) { isPrevAddBody = true; }
+		else {
+			COORD prevPos = { bodyPos.back().first, bodyPos.back().second };
+			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), prevPos);
+			std::cout << " " << std::endl;
+			bodyPos.pop_back();
+		}
+	}
+
+
+
+
+
+
+	return int32_t(0);
 }
--- moveRect/moveRectConsole/Actor.h
+++ moveRect/moveRectConsole/Actor.h
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <conio.h>
 #include <Windows.h>
+#include <deque>
+#include <algorithm>
+#include <chrono>
 
 namespace moveRectconsole {
 
@@ -22,7 +25,15 @@ namespace moveRectconsole {
 		int32_t move();
 		char getInput();
 
-		COORD pos;
+		int32_t saveBodyPosData();
+
+		COORD headPos;
+		std::deque<std::pair<uint32_t, uint32_t>> bodyPos;
+		uint32_t currentBodyLength;
+		bool isPrevAddBody;
+		const uint32_t maxBodyLength = 50;
+		std::chrono::system_clock::time_point prevFrameTime;
+		
 		char input;
 	};
 }
